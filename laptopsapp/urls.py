068b4d6ab849f3fa4cb5c7d953127e3eb0010522
Cristian_Altamirano <cristianaltamiranoshadow@gmail.com>
2024-08-15 12:53:54 -0500

    laptops

--- laptopsapp/urls.py
+++ laptopsapp/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tiendadeguitarras project.
+URL configuration for laptopsapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -18,13 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from tiendadeguitarras import settings
+from laptopsapp import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('laptops.urls'))
+    path('', include('laptops.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
